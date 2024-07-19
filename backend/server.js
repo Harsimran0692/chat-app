e05a18552de041fbc54218c3fc808d93172a4d42
harsimran06 <harsimran@vdo.ai>
2024-07-19 18:13:20 -0400

    setting server.je

--- backend/server.js
+++ backend/server.js
@@ -2,92 +2,89 @@ const env = require('dotenv');
 env.config();
 
 const express = require('express');
+const path = require('path');
+const cors = require('cors');
 const app = express();
-const chats = require('./data/chats.js');
+
 const connectDB = require('./config/db.js');
 const userRoutes = require('./routes/userRoutes.js');
 const chatRoutes = require('./routes/chatRoutes.js');
 const messageRoute = require('./routes/messageRoute.js');
-const path = require('path');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware.js');
-const cors = require('cors');
 
+// Connect to MongoDB
 connectDB();
 
-app.use(express.json()); // to accept json data
+// Middleware
+app.use(express.json()); // to accept JSON data
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("Home");
-    res.end();
-});
+// Define API routes
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoute)
-app.use(notFound);
-app.use(errorHandler);
+app.use('/api/message', messageRoute);
 
-// --------------------------deployment------------------------------
-
+// Serve static files from the React app in production
 const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, 'frontend/build')));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))
     );
 } else {
-    app.get("/", (req, res) => {
-        res.send("API is running..");
+    app.get('/', (req, res) => {
+        res.send('API is running..');
     });
 }
 
-// --------------------------deployment------------------------------
+// Error handling middleware
+app.use(notFound);
+app.use(errorHandler);
 
+// Start the server
 const PORT = process.env.PORT || 5001;
-
 const server = app.listen(PORT, (err) => {
     if (err) console.error(err);
     console.log(`Listening at PORT: ${PORT}`);
 });
 
-const io = require("socket.io")(server, {
+// Socket.io configuration
+const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000",
-        // credentials: true,
+        origin: 'http://localhost:3000',
     },
 });
 
-io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-    socket.on("setup", (userData) => {
+io.on('connection', (socket) => {
+    console.log('Connected to socket.io');
+    socket.on('setup', (userData) => {
         socket.join(userData._id);
-        socket.emit("connected");
+        socket.emit('connected');
     });
 
-    socket.on("join chat", (room) => {
+    socket.on('join chat', (room) => {
         socket.join(room);
-        console.log("User Joined Room: " + room);
+        console.log('User Joined Room: ' + room);
     });
-    socket.on("typing", (room) => socket.in(room).emit("typing"));
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+    socket.on('typing', (room) => socket.in(room).emit('typing'));
+    socket.on('stop typing', (room) => socket.in(room).emit('stop typing'));
 
-    socket.on("new message", (newMessageRecieved) => {
-        var chat = newMessageRecieved.chat;
+    socket.on('new message', (newMessageReceived) => {
+        var chat = newMessageReceived.chat;
 
-        if (!chat.users) return console.log("chat.users not defined");
+        if (!chat.users) return console.log('chat.users not defined');
 
         chat.users.forEach((user) => {
-            if (user._id == newMessageRecieved.sender._id) return;
+            if (user._id == newMessageReceived.sender._id) return;
 
-            socket.in(user._id).emit("message recieved", newMessageRecieved);
+            socket.in(user._id).emit('message received', newMessageReceived);
         });
     });
 
-    socket.off("setup", () => {
-        console.log("USER DISCONNECTED");
+    socket.off('setup', () => {
+        console.log('USER DISCONNECTED');
         socket.leave(userData._id);
     });
 });
